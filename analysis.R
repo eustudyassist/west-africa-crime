358f34ec92b02b6616261fbf638c754f18972a2f
eustudyassist <eustudyassist@gmail.com>
2026-09-13 07:31:17 -0700

    Created table 1 data:
    Added the following variables.
    - country
    - city
    - n_crime

---
+++ analysis.R
@@ -0,0 +1,17 @@
+
+# Create objects ----------------------------------------------------------
+
+country <- c(rep("Nigeria", 3), "Cameroon", rep("Nigeria", 3), 
+  "Cameroon", rep("Nigeria", 8), "Cameroon", rep("Nigeria", 3))
+
+length(country)
+unique(country)
+
+city <- c("Maiduguri", "Unknown", "Kano", "Bamenda", "Port Harcourt",
+          "Potiskum", "Damaturu", "Unknown", "Jos", "Lagos", "Gombe", "Kaduna",
+          "Logo district", "Konduga", "Warri", "Bama", "Buea", "Buni Yadi",
+          "Damboa", "Gamboru"
+          )
+
+n_crime <- c(488, 225, 130, 87, 76, 65, 63, 55, 51, 46, 38, 37, 36, 35, 32,
+             31, 30, 30, 29)
